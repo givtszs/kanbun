508a78ddae53b49924ad004908d14b13fc04c6f1
givtszs <givtszs@gmail.com>
2024-01-07 19:21:59 +0200

    Update registration prompt fragment implementation, theme and color res.

--- app/src/main/java/com/example/kanbun/presentation/registration/RegistrationPromptFragment.kt
+++ app/src/main/java/com/example/kanbun/presentation/registration/RegistrationPromptFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.kanbun.R
 import com.example.kanbun.databinding.FragmentRegistrationPromptBinding
 
 class RegistrationPromptFragment : Fragment() {
@@ -17,6 +19,13 @@ class RegistrationPromptFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentRegistrationPromptBinding.inflate(inflater, container, false)
+        setStatusBarColor(ContextCompat.getColor(requireContext(), R.color.md_theme_light_primary_variant))
         return binding.root
+    }
+
+    private fun setStatusBarColor(color: Int) {
+        requireActivity().window.apply {
+            statusBarColor = color
+        }
     }
 }
