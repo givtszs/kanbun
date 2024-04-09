707f323986fb26d3c9d3900c313dbfdbd46cad21
givtszs <givtszs@gmail.com>
2024-04-09 23:46:12 +0300

    Add extension functions to retrieve the data of the Result wrapper.

--- app/src/main/java/com/example/kanbun/common/Result.kt
+++ app/src/main/java/com/example/kanbun/common/Result.kt
@@ -1,11 +1,27 @@
 package com.example.kanbun.common
 
-sealed class Result<out T : Any> {
-    class Success<out T : Any>(val data: T) : Result<T>()
-    class Error(val message: String?, val e: Throwable? = null) : Result<Nothing>()
+sealed class Result<T> {
+    class Success<T>(val data: T) : Result<T>()
+    class Error<T>(val message: String?, val e: Throwable? = null) : Result<T>()
+
+    fun onSuccess(block: (T) -> Unit): Result<T> {
+        if (this is Success) {
+            block(data)
+        }
+        return this
+    }
+
+    fun onError(block: (String?, Throwable?) -> Unit): Result<T> {
+        if (this is Result.Error) {
+            block(message, e)
+        }
+        return this
+    }
 }
 
-inline fun <T, R : Any> T.runCatching(block: T.() -> R): Result<R> {
+
+
+inline fun <T, R> T.runCatching(block: T.() -> R): Result<R> {
     return try {
         Result.Success(block())
     } catch (e: Throwable) {
